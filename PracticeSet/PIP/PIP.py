17b6914443f4c9aca987905b0d58ac550cc7ca90
Atharva Deshmukh <92073807+Atha-rva@users.noreply.github.com>
2026-02-09 11:19:47 +0530

    Added a Concept of a PIP ( Python in it )

---
+++ PracticeSet/PIP/PIP.py
@@ -0,0 +1,71 @@
+# What is PIP?
+
+# PIP is a package manager for Python packages, or modules if you like.
+
+# Note: If you have Python version 3.4 or later, PIP is included by default.
+
+# What is a Package?
+
+# A package contains all the files you need for a module.
+
+# Modules are Python code libraries you can include in your project.
+
+
+# Download a Package
+
+# Downloading a package is very easy.
+
+# Open the command line interface and tell PIP to download the package you want.
+
+# Navigate your command line to the location of Python's script directory, and type the following:
+
+# C:\Users\Your Name\AppData\Local\Programs\Python\Python36-32\Scripts>pip install package_name
+
+
+# import camelcase
+
+# c = camelcase.CamelCase()
+
+# txt = "hello world"
+
+# print(c.hump(txt))
+
+
+# Find Packages
+
+# Find more packages at https://pypi.org/.
+
+
+# Remove a Package
+
+# Use the uninstall command to remove a package:
+
+# pip uninstall camelcase
+
+# The PIP Package Manager will ask you to confirm that you want to remove the camelcase package:
+
+# Uninstalling camelcase-02.1:
+#   Would remove:
+#     c:\users\Your Name\appdata\local\programs\python\python36-32\lib\site-packages\camelcase-0.2-py3.6.egg-info
+#     c:\users\Your Name\appdata\local\programs\python\python36-32\lib\site-packages\camelcase\*
+# Proceed (y/n)?
+
+
+# List Packages
+# Use the list command to list all the packages installed on your system:
+
+# Example
+# List installed packages:
+
+# C:\Users\Your Name\AppData\Local\Programs\Python\Python36-32\Scripts>pip list
+# Result:
+
+# Package         Version
+# -----------------------
+# camelcase       0.2
+# mysql-connector 2.1.6
+# pip             18.1
+# pymongo         3.6.1
+# setuptools      39.0.1
+
+
